23266c161bc9af4369de5572ccbbe91a1cfef9f6
Jeslin1231 <131727231+Jeslin1231@users.noreply.github.com>
2023-04-26 09:39:33 -0700

    Add files via upload

---
+++ server/Routes/PostRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getTimelinePosts, likePost, deletePost, updatePost, getPost, createPost } from "../Controllers/PostController.js";
+
+const router = express.Router()
+
+
+router.post('/', createPost)
+router.get('/:id', getPost)
+router.put('/:id', updatePost)
+router.delete("/:id", deletePost)
+router.put("/:id/like", likePost)
+router.get("/:id/timeline", getTimelinePosts)
+export default router;
